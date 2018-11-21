7adf335ccd4aaf0765e4c534a3953f0c7e80dea5
ama.michelange <ama-michelange@users.noreply.github.com>
2018-11-21 02:21:57 +0100

    Add Full Title Pipe

--- app/src/app/things/things.module.ts
+++ app/src/app/things/things.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { MaterialModule } from "../shared";
 import { ThingsComponent } from "./components/things.component";
+import { FullTitlePipe } from "./pipes";
 
 @NgModule({
    imports: [CommonModule, MaterialModule],
-   declarations: [ThingsComponent],
+   declarations: [ThingsComponent, FullTitlePipe],
    exports: [ThingsComponent]
 })
 export class ThingsModule {}
