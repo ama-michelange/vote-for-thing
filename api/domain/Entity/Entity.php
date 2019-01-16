b2505815dcce2795d0ca90052c1c23bc6548a77e
ama.michelange <ama-michelange@users.noreply.github.com>
2019-01-17 00:54:08 +0100

    Use associated foreign keys to build fillable, hidden and visible attributes to prepare store to repository

--- api/domain/Entity/Entity.php
+++ api/domain/Entity/Entity.php
@@ -4,7 +4,10 @@
 namespace Domain\Entity;
 
 
+use BadMethodCallException;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use UnexpectedValueException;
 
 abstract class Entity extends Model
 {
@@ -17,15 +20,18 @@ abstract class Entity extends Model
     */
    public function __construct(array $attributes = [])
    {
-      parent::__construct($attributes);
+      // It's important to call the next method before the parent constructor 
+      $this->pushAssociatedForeignKey();
       $this->buildDefaultVisible();
+
+      parent::__construct($attributes);
    }
 
    /**
     * Return the associated entity names.
     * @return array
     */
-   public function getAssociated():array
+   public function getAssociated() : array
    {
       return $this->associated;
    }
@@ -42,12 +48,25 @@ abstract class Entity extends Model
    }
 
    /**
+    * Push the associated Foreign Keys to fillable and hidden attributes.
+    */
+   protected function pushAssociatedForeignKey() : void
+   {
+      $keys = $this->findAllAssociatedForeignKey();
+      $t = array_merge($this->getFillable(), $keys);
+      $this->fillable($t);
+
+      $this->addHidden($keys);
+   }
+
+
+   /**
     * Build the default visible attributes.
     */
    protected function buildDefaultVisible() : void
    {
       if (count($this->visible) === 0) {
-         $visible = $this->getFillable();
+         $visible = array_diff($this->getFillable(), $this->getHidden());
          $visible[] = $this->getKeyName();
          if ($this->timestamps) {
             $visible[] = $this->getCreatedAtColumn();
@@ -55,6 +74,43 @@ abstract class Entity extends Model
          }
          $this->setVisible($visible);
       }
+   }
+
+   /**
+    * Find all foreign keys of the associated entities.
+    * @return array The name of the foreign keys
+    * @throws BadMethodCallException If an associated entity don't exist
+    * @throws UnexpectedValueException If an associated entity is not implemented
+    */
+   public function findAllAssociatedForeignKey() : array
+   {
+      $ret = array();
+      foreach ($this->getAssociated() as $val) {
+         $ret[] = $this->findAssociatedForeignKey($val);
+      }
+      return $ret;
+   }
+
+   /**
+    * Find the foreign key of an associated entity.
+    * @param string $associated The associated entity
+    * @return string The foreign key
+    * @throws BadMethodCallException If an associated entity don't exist
+    * @throws UnexpectedValueException If an associated entity is not implemented
+    */
+   public function findAssociatedForeignKey(string $associated) : string
+   {
+      $forKey = null;
+      $belong = $this->$associated();
+      switch (get_class($belong)) {
+         case BelongsTo::class :
+            $forKey = $belong->getForeignKey();
+            break;
+         default:
+            $mess = sprintf('Class not found "%s" for this associated attribute "%s"', get_class($belong), $associated);
+            throw new UnexpectedValueException($mess);
+      }
+      return $forKey;
    }
 
 }
